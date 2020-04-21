cbb586cb479a5ba916f0ec1ad82b34d3340f21df
dipesh digwal <dipeshdigwal1@icloud.com>
2020-04-21 13:35:57 +0530

    series

--- series.py
+++ series.py
@@ -1,17 +1,17 @@
-from math import pow,sin,pi
+from math import pow
+
+
+# find the factorial of a integer n.
 def factorial(n):
-    p=1
-    for i in range(2,n):
-        p*=i
+    p = 1
+    for i in range(2, n+1):
+        p *= i
     return(p)
 
-def sin_2(x,n):
-    s=0
-    for i in range(n):
-        s+=(pow(-1,i)*pow(x,(2*i)+1))/factorial((2*i)+1)
-    return(s)
 
-x=pi/20
-n=2
-k=(sin(x)-sin_2(x,n))/sin(x)
-print(str(k))
+# find sin of a number
+def sin_2(x, n):
+    s = 0
+    for i in range(n):
+        s += (pow(-1, i)*pow(x, (2*i)+1))/factorial((2*i)+1)
+    return(s)
